b21ea83b85200f3c73a2e8c4595503916200f5b0
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-09 02:15:44 +0300

    feat: manage chatbot typing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,3 +2,9 @@
 const chat = document.getElementById("body") as HTMLElement;
 const inputField = document.getElementById("user-input") as HTMLInputElement;
 const sendBttn = document.getElementById("send") as HTMLButtonElement;
+
+// initialize variables to manage chatbot typing indicator and interval
+
+let isBotTyping = false;
+let typingInterval: ReturnType<typeof setInterval> | null = null;
+let typingMessage = "Typing";
