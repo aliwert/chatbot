caff306fee49c34bc771b3c1b75e502151ff4281
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-09 13:58:25 +0300

    feat: display bot message

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,10 +13,31 @@ let typingMessage = "Typing";
 
 const addUserMessage = (message: string): void => {
   const userMsg = document.createElement("div");
-  userMsg.className = "user-msg";
+  userMsg.className = "user-message";
   userMsg.innerText = message;
   chat.appendChild(userMsg);
 
   //scroll to bottom of the chat show the latest message
+  chat.scrollTop = chat.scrollHeight;
+};
+
+// display the bot message in chat
+const addBotMessage = (message: string): void => {
+  if (isBotTyping) {
+    if (typingInterval) {
+      clearInterval(typingInterval);
+    }
+    const typingIndicator = chat.querySelector(
+      ".typing-indicator"
+    ) as HTMLElement;
+    if (typingIndicator) {
+      chat.removeChild(typingIndicator);
+    }
+    isBotTyping = false;
+  }
+  const chatbotMessage = document.createElement("div");
+  chatbotMessage.className = "message";
+  chatbotMessage.innerText = message;
+  chat.appendChild(chatbotMessage);
   chat.scrollTop = chat.scrollHeight;
 };
