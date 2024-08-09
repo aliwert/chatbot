3c3558355ab85282fa5f40315ded25c4c1def84e
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-09 13:49:47 +0300

    feat: display user message

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,3 +8,15 @@ const sendBttn = document.getElementById("send") as HTMLButtonElement;
 let isBotTyping = false;
 let typingInterval: ReturnType<typeof setInterval> | null = null;
 let typingMessage = "Typing";
+
+// display the user message in chat
+
+const addUserMessage = (message: string): void => {
+  const userMsg = document.createElement("div");
+  userMsg.className = "user-msg";
+  userMsg.innerText = message;
+  chat.appendChild(userMsg);
+
+  //scroll to bottom of the chat show the latest message
+  chat.scrollTop = chat.scrollHeight;
+};
